ac62273fe6ca6161d20233a134d00264da1eb67b
Vipin <kirthane.vipin@gmail.com>
2019-12-17 12:10:20 -0500

    Added hamburger menu

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,7 @@
 var scroll = new SmoothScroll('a[href*="#"]', {
     offset: 70,
 });
+
+$(".menuOpener").click(function () {
+    $(".menuOpener, .menuOpenerInner, .menu").toggleClass("active");
+});
