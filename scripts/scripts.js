4b6ab7e91aea5c1f9e3e82137d784469f919bcb2
Vipin <kirthane.vipin@gmail.com>
2020-01-17 12:59:42 -0500

    Added Resume Button

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,12 +2,23 @@ AOS.init({
   duration: 1200
 });
 
+// AOS.init({
+//   disable: 'mobile'
+// });
+
+AOS.init({
+  disable: function () {
+    var maxWidth = 760;
+    return window.innerWidth < maxWidth;
+  }
+});
+
 var scroll = new SmoothScroll('a[href*="#"]', {
   offset: 70
 });
 
-$(function() {
-  $(".menuOpener").on("click keypress", function() {
+$(function () {
+  $(".menuOpener").on("click keypress", function () {
     $(".menuOpener, .menuOpenerInner, .menu").toggleClass("active");
   });
 });
