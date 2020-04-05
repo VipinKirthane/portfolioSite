de84e6d2caddd801e1fc4d25357dc8ee6fd38274
Vipin <kirthane.vipin@gmail.com>
2020-04-05 15:34:55 -0400

    Added AOS back again

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,14 @@
 AOS.init({
-  duration: 1200
+  duration: 1200,
+  disable: "phone",
 });
 
 var scroll = new SmoothScroll('a[href*="#"]', {
-  offset: 70
+  offset: 70,
 });
 
-$(function() {
-  $(".menuOpener").on("click keypress", function() {
+$(function () {
+  $(".menuOpener").on("click keypress", function () {
     $(".menuOpener, .menuOpenerInner, .menu").toggleClass("active");
   });
 });
